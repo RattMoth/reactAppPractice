44fdf63b8aeae570d0e07ad8676fc2ae7e3129ef
Matt <mattanator94@gmail.com>
2019-01-03 17:22:11 -0800

    Added click event to Card.js. Easier than expected!

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Card = ({ name, email, id }) => {
+  const clicky = () => console.log('Click!');
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+    <div
+      onClick={clicky}
+      className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc"
+    >
       <img src={`https://robohash.org/${id}?size=200x200`} alt="profile" />
       <div>
         <h2>{name}</h2>
