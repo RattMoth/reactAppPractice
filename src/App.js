20988329f9ba57783856db3f4dfb5b178b813c62
Matt <mattanator94@gmail.com>
2019-01-04 02:07:15 -0800

    Updated App.js to use componentDidMount and pull the user list from jsonplaceholder.com instead of locally import from robots.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBar from './SearchBar';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   onSearchChange = event => {
@@ -21,6 +26,9 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot =>
       robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
     );
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">Loading...</h1>;
+    }
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
