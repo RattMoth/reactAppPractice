791f948c3541e13066d75a4b1ce63e9b52debadb
Matt <mattanator94@gmail.com>
2019-01-19 23:18:18 -0800

    Updates

---
+++ precache-manifest.5b90bf62ccbfd4a4210cc9f0d31f409b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e4ed29baed4c12805f9",
+    "url": "/reactAppPractice/static/js/runtime~main.4e4ed29b.js"
+  },
+  {
+    "revision": "df404b22a83a84ba83db",
+    "url": "/reactAppPractice/static/js/main.df404b22.chunk.js"
+  },
+  {
+    "revision": "023ad2826a379cb595c7",
+    "url": "/reactAppPractice/static/js/1.023ad282.chunk.js"
+  },
+  {
+    "revision": "df404b22a83a84ba83db",
+    "url": "/reactAppPractice/static/css/main.e2a710a7.chunk.css"
+  },
+  {
+    "revision": "023ad2826a379cb595c7",
+    "url": "/reactAppPractice/static/css/1.e1c786b6.chunk.css"
+  },
+  {
+    "revision": "73070adbfc772a3b33ac78ffb19323d2",
+    "url": "/reactAppPractice/index.html"
+  }
+];
